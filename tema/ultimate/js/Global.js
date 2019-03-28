84080e589392ea30bf4fe4f45afd56707f8b6889
recephurol <recephurol@hotmail.com>
2019-03-28 09:12:50 +0300

    celikkalip

---
+++ tema/ultimate/js/Global.js
@@ -0,0 +1,25 @@
+﻿
+    $(document).on("click", "#btnMailGonder", function () {
+        var Contact = new Object();
+        Contact.AdiSoyadi = $("#AdiSoyadi").val();
+        Contact.EMail = $("#EMail").val();
+        Contact.Telefon = $("#Telefon").val();
+        Contact.Mesaj = $("#Mesaj").val();
+        Contact.Konu = $("#Konu").val();
+
+        $.ajax({
+            url: "/Home/MailIndex",
+            data: Contact,
+            type: "POST",
+            dataType: 'json',
+            success: function (response) {
+                if (response.Success) {
+                    swal("Tebrikler!", "Mail gönderimi Başarılı", "success");
+                }
+                else {
+                    swal("Üzgünüm!", "Mail gönderimi Başarısız", "error");
+                } 
+                    
+            }
+        })
+    })
